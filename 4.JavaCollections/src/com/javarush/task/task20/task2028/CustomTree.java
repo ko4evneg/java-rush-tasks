a858f715b27c1d9fb4d25f80fa951b6d260a72a0
ko4evneg <ko4evneg@gmail.com>
2020-12-15 11:41:41 +0300

    task20.task2028 Построй дерево
    CustomTree, added methods: remove
    CustomTree, modified methods: add

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -5,16 +5,57 @@ import java.util.*;
 
 /* 
 Построй дерево(1)
+Тебе необходимо:
+1) переопределить метод add(String s) - добавляет элементы дерева, в качестве параметра принимает имя элемента (elementName), искать место для вставки начинаем слева направо.
+2) переопределить метод size() - возвращает текущее количество элементов в дереве.
+3) реализовать метод getParent(String s) - возвращает имя родителя элемента дерева, имя которого было полученного в качестве параметра.
+
+Если необходимо, можешь вводить дополнительные методы и поля, не указанные в задании.
+
+
+Требования:
+2. В классе CustomTree должны быть переопределены методы add(String s) и size().
+3. После добавления N элементов в дерево с помощью метода add, метод size должен возвращать N.
+4. Метод getParent должен возвращать имя родителя для любого элемента дерева.
 */
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
-    Entry<String> root;
-    int size = 0;
-    private ArrayList<Entry<String>> tree = new ArrayList<>();
+    private Entry<String> root;
+    private int size = 0;
+    private final ArrayList<Entry<String>> tree = new ArrayList<>();
 
     public CustomTree() {
         root = new Entry<>("0");
         tree.add(root);
+    }
+
+    public boolean remove(Object o) {
+        if (!(o instanceof String))
+            throw new UnsupportedOperationException();
+        for (Entry<String> e : tree)
+            if (e.elementName.equals(o)) {
+                toDel(e);
+                return true;
+            }
+        return false;
+    }
+
+    private void toDel(Entry<String> entry) {
+        if (entry.rightChild != null) {
+            toDel(entry.rightChild);
+            entry.rightChild = null;
+        }
+        if (entry.leftChild != null) {
+            toDel(entry.leftChild);
+            entry.leftChild = null;
+        }
+        tree.remove(entry);
+        if (entry.parent.leftChild == entry)
+            entry.parent.leftChild = null;
+        else
+            entry.parent.rightChild = null;
+        size--;
+        //System.out.println("Removed: " + entry.elementName);
     }
 
     @Override
@@ -27,8 +68,8 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     public boolean add(String s) {
         Entry<String> newEntry = new Entry<>(s);
         if (s != null) {
-            for (int i = tree.size() - 1; i >= 0; i--) {
-                if ((i > 0 && !tree.get(i - 1).isAvailableToAddChildren()) || i == 0) {
+            for (int i = 0; i < tree.size(); i++) {
+                if (tree.get(i).isAvailableToAddChildren()) {
                     tree.add(newEntry);
                     newEntry.parent = tree.get(i);
                     if (tree.get(i).availableToAddLeftChildren) {
@@ -40,8 +81,14 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
                     }
                     size++;
                     return true;
-                } else {
-
+                } else if (i == tree.size() - 1){
+                    for (Entry<String> stringEntry : tree) {
+                        if (stringEntry.leftChild == null && !stringEntry.availableToAddLeftChildren)
+                            stringEntry.availableToAddLeftChildren = true;
+                        if (stringEntry.rightChild == null && !stringEntry.availableToAddRightChildren)
+                            stringEntry.availableToAddRightChildren = true;
+                    }
+                    add(s);
                 }
             }
         }
