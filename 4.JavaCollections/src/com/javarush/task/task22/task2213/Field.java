0524b366dfcc63b415e275ac71ba16910ce48c5d
ko4evneg <ko4evneg@gmail.com>
2020-12-17 19:37:29 +0300

    task22.task2213;
    Tetris (12)

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task22.task2213;
+
+public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.height = height;
+        this.width = width;
+        this.matrix = new int[height][width];
+    }
+
+    public void print() {
+    }
+
+    public void removeFullLines() {
+    }
+
+    public Integer getValue(int x, int y) {
+        return null;
+    }
+
+    public void setValue(int x, int y, int value) {
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+}
