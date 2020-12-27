0893dc846423648c1e91eff8c20dbf91b28e8a81
ko4evneg <ko4evneg@gmail.com>
2020-12-27 16:56:07 +0300

    task22.task2201
    Строки нитей или строковые нити? Вот в чем вопрос

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2201/StringForSecondThreadTooShortException.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task22.task2201;
+
+public class StringForSecondThreadTooShortException extends RuntimeException {
+    public StringForSecondThreadTooShortException() {
+    }
+
+    public StringForSecondThreadTooShortException(Throwable cause) {
+        super(cause);
+    }
+}
