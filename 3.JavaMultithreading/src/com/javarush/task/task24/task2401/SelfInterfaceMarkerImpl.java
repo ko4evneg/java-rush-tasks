3da21880ef882474947a6b15dfe7804bb46517fe
ko4evneg <ko4evneg@gmail.com>
2021-01-05 17:32:01 +0300

    task24.task2413
    Создание своего интерфейса-маркера

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void any_one(){
+
+    }
+
+    public void any_two(){
+
+    }
+}
