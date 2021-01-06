68ec73b50f0b925e0501259a901892f60bd1c60a
ko4evneg <ko4evneg@gmail.com>
2021-01-06 18:46:52 +0300

    task20.task2021
    Сериализация под запретом

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/* 
+Сериализация под запретом
+*/
+
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution  {
+        public void writeObject(ObjectOutputStream out) throws NotSerializableException {
+            throw new NotSerializableException();
+        }
+        public void readObject(ObjectInputStream in) throws NotSerializableException{
+            throw new NotSerializableException();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            ObjectOutputStream in = new ObjectOutputStream (new ByteArrayOutputStream());
+            in.writeObject(new SubSolution());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}
