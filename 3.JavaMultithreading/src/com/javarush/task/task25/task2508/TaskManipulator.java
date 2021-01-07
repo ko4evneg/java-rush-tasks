d596acb3262d5fb5f8f82a9c395576c62b44e6b4
ko4evneg <ko4evneg@gmail.com>
2021-01-07 20:25:31 +0300

    task25.task2508
    Не валять дурака

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task25.task2508;
+
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+    Thread thread;
+    String threadName;
+
+    @Override
+    public void start(String threadName) {
+        this.threadName = threadName;
+        thread = new Thread(this,threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!thread.isInterrupted()) {
+                System.out.println(threadName);
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {}
+    }
+}
