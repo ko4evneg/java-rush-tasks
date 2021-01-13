812cef8df3b4be616c0d84cd85708ccfc1d6f1cc
ko4evneg <ko4evneg@gmail.com>
2021-01-14 02:20:20 +0300

    com.javarush.task.task30.3008
    Чат(7)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,44 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Server {
-    public static void main(String[] args) {
-        
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry entry : connectionMap.entrySet()) {
+            Connection c = (Connection) entry.getValue();
+            try {
+                c.send(message);
+            } catch (IOException ioException) {
+                ConsoleHelper.writeMessage("Error while sending message");
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Server has run");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("IO error in serverSocket");
+        }
     }
 }
