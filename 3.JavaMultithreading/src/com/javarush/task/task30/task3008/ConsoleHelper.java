784544b618ebaf137a0b1dbfb32c40665cdb9523
ko4evneg <ko4evneg@gmail.com>
2021-01-14 00:09:49 +0300

    com.javarush.task.task30.3008
    Чат(2)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task30.task3008;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String string = null;
+        try {
+            string = reader.readLine();
+            return string;
+        } catch (IOException ioException) {
+            System.out.print("\nПроизошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            return readString();
+        }
+    }
+
+    public static int readInt() {
+        int num = 0;
+        try {
+            num = Integer.parseInt(readString());
+        } catch (NumberFormatException e) {
+            writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            num = Integer.parseInt(readString());
+        }
+        return num;
+    }
+}
