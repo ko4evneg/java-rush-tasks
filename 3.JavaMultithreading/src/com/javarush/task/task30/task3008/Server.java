262d45aad519949f87cbadb8f099f36bc939bca4
ko4evneg <ko4evneg@gmail.com>
2021-01-14 17:34:19 +0300

    com.javarush.task.task30.3008
    Чат(8)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -15,6 +15,18 @@ public class Server {
         public Handler(Socket socket) {
             this.socket = socket;
         }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message answer = connection.receive();
+                if (answer.getType() == MessageType.USER_NAME && !answer.getData().isEmpty() && !connectionMap.containsKey(answer.getData())) {
+                    connectionMap.put(answer.getData(), connection);
+                    connection.send(new Message(MessageType.NAME_ACCEPTED));
+                    return answer.getData();
+                }
+            }
+        }
     }
 
     public static void sendBroadcastMessage(Message message) {
