eef709fb49545fb888bbb458656f739a0afafda4
ko4evneg <ko4evneg@gmail.com>
2021-01-16 19:05:51 +0300

    com.javarush.task.task31.3110
    Archiver (10)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,19 +1,13 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-//Main class
 public class Archiver {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         Operation operation = null;
-        while (operation != Operation.EXIT) {
+        do {
             try {
                 operation = askOperation();
                 CommandExecutor.execute(operation);
@@ -22,15 +16,20 @@ public class Archiver {
             } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
-        }
+        } while (operation != Operation.EXIT);
     }
 
-    //Method for requesting needed operation
+
     public static Operation askOperation() throws IOException {
-        ConsoleHelper.writeMessage("Select operation type:");
-        for (Operation operation : Operation.values()) {
-            ConsoleHelper.writeMessage(operation.ordinal() + " - " + operation.name());
-        }
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
         return Operation.values()[ConsoleHelper.readInt()];
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,54 +1,56 @@
 package com.javarush.task.task31.task3110;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-//Class for performing operations upon an archive.
 public class ZipFileManager {
-    //Archive absolute path
-    private Path zipFile;
+    // Полный путь zip файла
+    private final Path zipFile;
 
     public ZipFileManager(Path zipFile) {
         this.zipFile = zipFile;
     }
 
-    //Archives source
     public void createZip(Path source) throws Exception {
+        Path zipDirectory = zipFile.getParent();
+        if (Files.notExists(zipDirectory))
+            Files.createDirectories(zipDirectory);
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
-            String sourceFileName = source.getFileName().toString();
-            ZipEntry zipEntry = new ZipEntry(sourceFileName);
-            zipOutputStream.putNextEntry(zipEntry);
-            try (BufferedInputStream fileBIS = new BufferedInputStream(Files.newInputStream(source))) {
-                byte[] buffer = new byte[4096];
-                int bytesRead = -1;
-                while ((bytesRead = fileBIS.read(buffer)) != -1) {
-                    zipOutputStream.write(buffer, 0, bytesRead);
-                }
+            if (Files.isDirectory(source)) {
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+                for (Path fileName : fileNames)
+                    addNewZipEntry(zipOutputStream, source, fileName);
+            } else if (Files.isRegularFile(source)) {
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            } else {
+                throw new PathIsNotFoundException();
             }
+        }
+    }
+
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
+        Path fullPath = filePath.resolve(fileName);
+        try (InputStream inputStream = Files.newInputStream(fullPath)) {
+            ZipEntry entry = new ZipEntry(fileName.toString());
+            zipOutputStream.putNextEntry(entry);
+            copyData(inputStream, zipOutputStream);
             zipOutputStream.closeEntry();
         }
     }
-/*
-4. Перепиши данные из файла, который архивируем в поток архива. Для этого:
-4.1. Создай поток InputStream для добавляемого файла source, используя метод newInputStream класса Files
-4.2. Сделай цикл, который будет читать данные из InputStream (созданного в п.4.1), пока они там есть и записывать их в ZipOutputStream (созданный в п.1)
-4.3. Закрой InputStream, сделай это с помощью try-with-resources
-5. Закрой элемент архива у потока архива
-6. Закрой поток архива, сделай это также с помощью try-with-resources
-7. Запусти программу и проверь, что файл архивируется
 
-
-Требования:
-1. Метод createZip должен создавать ZipOutputStream используя поле zipFile и метод Files.newOutputStream.
-2. Метод createZip должен создавать элемент архива ZipEntry c именем файла, полученным из параметра source.
-3. Созданный ZipEntry нужно добавить в ZipOutputStream.
-4. Для переменной source должен быть создан InputStream с помощью метода Files.newInputStream.
-5. Данные из InputStream нужно переписать в ZipOutputStream.
-6. Закрой текущий Entry у объекта ZipOutputStream.
-7. InputStream для source должен быть закрыт.
-8. ZipOutputStream должен быть закрыт.*/
+    private void copyData(InputStream in, OutputStream out) throws Exception {
+        byte[] buffer = new byte[8 * 1024];
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+        }
+    }
 }
